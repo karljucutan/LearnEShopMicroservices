107709063de2fed8285cd0833b5e8085d3cf8f6a
karljucutan <karljucutan@gmail.com>
2024-10-27 16:50:21 +0800

    Add Registeration of Carter and MediatR in the Catalog.API

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
