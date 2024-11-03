879cd672aec71623557715bb35db9670cc8f3c53
karljucutan <karljucutan@gmail.com>
2024-11-03 20:37:37 +0800

    Add GetProducts and GetProductById (WIP)

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(string message = "Product not found!") : base(message) { }
+
+        public static void ThrowIfNull([NotNull] object? product)
+        {
+            if (product is null)
+                throw new ProductNotFoundException();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+
+    public class GetProductByIdQueryHandler(
+        IDocumentSession session,
+        ILogger logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            ProductNotFoundException.ThrowIfNull(product);
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
