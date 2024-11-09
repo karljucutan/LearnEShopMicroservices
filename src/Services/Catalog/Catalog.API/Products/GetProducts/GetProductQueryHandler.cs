61d6df05f4ca683cca0461622461ca9174431df7
karljucutan <karljucutan@gmail.com>
2024-11-09 19:53:05 +0800

    Add Pagination using Marten.Pagination

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductQueryHandler.cs
@@ -1,6 +1,8 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Marten.Pagination;
+
+namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductResult>;
+    public record GetProductsQuery(int PageNumber = 1, int PageSize = 10) : IQuery<GetProductResult>;
     public record GetProductResult(IEnumerable<Product> Products);
 
     public class GetProductQueryHandler(IDocumentSession session)
@@ -8,7 +10,8 @@
     {
         public async Task<GetProductResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber, query.PageSize, cancellationToken);
 
             return new GetProductResult(products);
         }
