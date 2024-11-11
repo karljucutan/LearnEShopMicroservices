b2924dd614cd4ee93a7aeeb77dea5b353cdc83e4
karljucutan <karljucutan@gmail.com>
2024-11-11 20:55:22 +0800

    add get basket query handler

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -1,6 +1,16 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketQueryHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO: get basket from db
+            // var basket = await _repository.GetBasket(query.UserName);
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
     }
 }
