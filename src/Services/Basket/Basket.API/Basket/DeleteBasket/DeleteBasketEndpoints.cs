cf081f5136c1971df66d3717f234d256a4d69772
karljucutan <karljucutan@gmail.com>
2024-11-12 19:46:49 +0800

    fix delete basket endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,18 +1,15 @@
 ﻿namespace Basket.API.Basket.DeleteBasket
 {
-    public record DeleteBasketRequest(string UserName);
     public record DeleteBasketResponse(bool IsSuccess);
 
     public class DeleteBasketEndpoints : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("/basket",
-                async (DeleteBasketRequest request, ISender sender) =>
+            app.MapDelete("/basket/{userName}",
+                async (string userName, ISender sender) =>
                 {
-                    var command = request.Adapt<DeleteBasketCommand>();
-
-                    var result = await sender.Send(command);
+                    var result = await sender.Send(new DeleteBasketCommand(userName));
 
                     var response = result.Adapt<DeleteBasketResponse>();
 
