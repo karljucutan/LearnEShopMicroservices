76babaccb540b2b1d13e2cea86781e4d7da252ef
karljucutan <karljucutan@gmail.com>
2024-11-16 16:15:20 +0800

    * add efcore sqlite db * seed and auto migrate sqlite db * implement the crud for the discountservice grpc

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseDbMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountDbContext>();
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
