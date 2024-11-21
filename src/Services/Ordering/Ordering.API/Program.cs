242314f2b877c10bfe84915f72adcad519b5ded0
karljucutan <karljucutan@gmail.com>
2024-11-21 18:45:29 +0800

    add dependency injections between clean architecture layers

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+// Infrastructure
+// Application
+// API
+
+builder.Services.AddApplicationServices()
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
