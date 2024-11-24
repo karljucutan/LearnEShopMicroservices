b1ff5e04b4d396920265028404c4469c8e44c67d
karljucutan <karljucutan@gmail.com>
2024-11-24 20:16:16 +0800

    add order get features GetOrders, GetOrdersByCustomer, GetIOrdersByName

--- src/Services/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering.Application/GlobalUsings.cs
@@ -1,8 +1,13 @@
 ﻿global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Pagination;
 global using FluentValidation;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
+global using Ordering.Domain.Events;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
-
+global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Extensions;
