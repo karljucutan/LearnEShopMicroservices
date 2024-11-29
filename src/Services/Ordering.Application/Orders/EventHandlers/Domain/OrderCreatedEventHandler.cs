673beb7d9691cd15dc3600f3b98a21175c6dbe17
karljucutan <karljucutan@gmail.com>
2024-11-29 18:14:40 +0800

    add publishing of OrderCreated Domain Event for integration

--- src/Services/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,12 +1,19 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain
+﻿using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
 {
     //public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    public class OrderCreatedEventHandler(
+        ILogger<OrderCreatedEventHandler> logger,
+        IPublishEndpoint publishEndpoint)
         : INotificationHandler<OrderCreatedEvent>
     {
         public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
             logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+            var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
 
             //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
             //{
