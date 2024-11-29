a0ae9fb4b4bb2bcb5e6c2f3eeeb5eb4b3be74306
karljucutan <karljucutan@gmail.com>
2024-11-29 15:36:34 +0800

    add ordering health checks

--- src/Services/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -6,14 +7,17 @@ namespace Ordering.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddMediatR(config =>
             {
-                config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
                 config.AddOpenBehavior(typeof(ValidationBehavior<,>));
                 config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
+
+            //services.AddFeatureManagement();
+            //services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
             return services;
         }
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -12,9 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 // API
 
 builder.Services
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices();
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
