98e463de94a830fab67af05f6f53516d2271c1f3
karljucutan <karljucutan@gmail.com>
2024-11-30 17:19:15 +0800

    add Default interface method in IBasketService. add the css and images

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -2,7 +2,7 @@ namespace Shopping.Web.Pages;
 public class IndexModel
     (ICatalogService catalogService, IBasketService basketService, ILogger<IndexModel> logger)
     : PageModel
-{    
+{
     public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();    
 
     public async Task<IActionResult> OnGetAsync()
--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -13,5 +13,30 @@
 
         [Post("/basket-service/basket/checkout")]
         Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+        /// Default interface methods were introduced in C# 8, primarily as a way to make it easier to evolve an interface without breaking people that have implemented the interface.
+        /// The feature allows you to provide a method body when you define a method on an interface, similar to how you might use an abstract class.
+        public async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            // Get Basket If Not Exist Create New Basket with Default Logged In User Name: swn
+            var userName = "swn";
+            ShoppingCartModel basket;
+
+            try
+            {
+                var getBasketResponse = await GetBasket(userName);
+                basket = getBasketResponse.Cart;
+            }
+            catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
+            {
+                basket = new ShoppingCartModel
+                {
+                    UserName = userName,
+                    Items = []
+                };
+            }
+
+            return basket;
+        }
     }
 }
